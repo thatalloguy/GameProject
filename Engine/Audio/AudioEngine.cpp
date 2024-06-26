8d38c1271866b2bd9bd985a25e561530a7b956ac
thatalloguy <allosander@gmail.com>
2024-06-26 12:04:20 +0200

    Start on audio engine

---
+++ Engine/Audio/AudioEngine.cpp
@@ -0,0 +1,5 @@
+//
+// Created by allos on 6/26/2024.
+//
+
+#include "AudioEngine.h"
---
+++ Engine/Audio/AudioEngine.h
@@ -0,0 +1,14 @@
+//
+// Created by allos on 6/26/2024.
+//
+
+#ifndef GAME_AUDIOENGINE_H
+#define GAME_AUDIOENGINE_H
+
+
+class AudioEngine {
+
+};
+
+
+#endif //GAME_AUDIOENGINE_H
