236533948fd0aae5e7e3b3ffe9e4176736f512f7
thatalloguy <allosander@gmail.com>
2024-06-28 15:19:09 +0200

    Moving to a better structure

--- Engine/Audio/AudioEngine.cpp
+++ Engine/Audio/AudioEngine.cpp
@@ -1,109 +1,157 @@
-//
-// Created by allos on 6/26/2024.
-//
 
-#include <cstdlib>
-#include <spdlog/spdlog.h>
 #include "AudioEngine.h"
+#include <spdlog/spdlog.h>
 
-void Quack::AudioEngine::initialize() {
+void Quack::AudioEngine::binauralEffectProccessPCMFrames(ma_node *pNode, const float **ppFramesIn, ma_uint32 *pFrameCountIn, float **ppFramesOut, ma_uint32 *pFrameCountOut)  {
+    BinauralEffect* binauralEffect = (BinauralEffect*)pNode;
+    IPLBinauralEffectParams  binauralEffectParams;
+    IPLAudioBuffer inputBufferDesc;
+    IPLAudioBuffer outputBufferDesc;
+    ma_uint32 totalFramesToProcess = *pFrameCountOut;
+    ma_uint32 totalFramesProcessed = 0;
 
-    //INitialize SteamAudio
-    IPLContextSettings contextSettings{};
+    binauralEffectParams.direction.x = binauralEffect->direction.x;
+    binauralEffectParams.direction.y = binauralEffect->direction.y;
+    binauralEffectParams.direction.z = binauralEffect->direction.z;
+    binauralEffectParams.interpolation = IPL_HRTFINTERPOLATION_NEAREST;
+    binauralEffectParams.spatialBlend = 1.0f;
+    binauralEffectParams.hrtf = binauralEffect->HRTF;
+    binauralEffectParams.peakDelays = nullptr;
 
-    contextSettings.version = STEAMAUDIO_VERSION;
+    inputBufferDesc.numChannels = (IPLint32) ma_node_get_input_channels(pNode, 0);
 
-    _context = nullptr;
+    //use a loop in case the deinterleaved buffers are too small
+    outputBufferDesc.numSamples = binauralEffect->audioSettings.frameSize;
+    outputBufferDesc.numChannels = 2;
+    outputBufferDesc.data = binauralEffect->outBuffer;
 
-    IPLerror errorCode = iplContextCreate(&contextSettings, &_context);
-    if (errorCode) {
-        spdlog::error("Could not initialize steamaudio: {}", errorCode);
-        exit(-103);
+    while (totalFramesProcessed < totalFramesToProcess) {
+
+        ma_uint32  framesToProcessThisIteration = totalFramesToProcess - totalFramesProcessed;
+        if  (framesToProcessThisIteration > (ma_uint32)binauralEffect->audioSettings.frameSize) {
+            framesToProcessThisIteration = (ma_uint32)binauralEffect->audioSettings.frameSize;
+        }
+
+        if (inputBufferDesc.numChannels == 1) {
+            // no need for deinterleaving since its a mono stream.
+            binauralEffect->inBuffer[0] = (float*) ma_offset_pcm_frames_const_ptr_f32(ppFramesIn[0], totalFramesProcessed, 1);
+        } else {
+            // womp womp we need to deinterleave the input stream.
+            ma_deinterleave_pcm_frames(ma_format_f32, inputBufferDesc.numChannels, framesToProcessThisIteration,
+                                       ma_offset_pcm_frames_const_ptr_f32(ppFramesIn[0], totalFramesProcessed, inputBufferDesc.numChannels),
+                                       (void **) binauralEffect->inBuffer); // note the example doenst cast to (void **)
+        }
+
+        inputBufferDesc.data = binauralEffect->inBuffer;
+        inputBufferDesc.numSamples = (IPLint32) framesToProcessThisIteration;
+        // spannend
+        iplBinauralEffectApply(binauralEffect->iplEffect, &binauralEffectParams, &inputBufferDesc, &outputBufferDesc);
+
+        ma_interleave_pcm_frames(
+                ma_format_f32,
+                2,
+                framesToProcessThisIteration,
+                (const void **) binauralEffect->outBuffer, // note the example also doenst cast anything here.
+                ma_offset_pcm_frames_ptr_f32(ppFramesOut[0], totalFramesProcessed, 2)
+        );
+
+
+        totalFramesProcessed += framesToProcessThisIteration;
+    }
+
+    (void)pFrameCountIn; // unused?
+
+}
+
+Quack::BinauralEffectConfig Quack::AudioEngine::initBinauralEffectConfig(ma_uint32 channelsIn, IPLAudioSettings audioSettings, IPLContext context, IPLHRTF hrtf)  {
+    BinauralEffectConfig config;
+
+    MA_ZERO_OBJECT(&config);
+    config.nodeConfig = ma_node_config_init();
+    config.channelsIn = channelsIn;
+    config.audioSettings = audioSettings;
+    config.context = context;
+    config.HRTF = hrtf;
+
+    return config;
+}
+
+ma_result Quack::AudioEngine::initBinauralEffect(ma_node_graph* nodeGraph, const BinauralEffectConfig* config, const ma_allocation_callbacks* allocationCallbacks, BinauralEffect& binauralEffect) {
+    ma_result result;
+    ma_node_config baseConfig;
+    ma_uint32 channelsIn;
+    ma_uint32 channelsOut;
+    IPLBinauralEffectSettings iplBinauralEffectSettings;
+    size_t heapSizeInBytes;
+
+    MA_ZERO_OBJECT(&binauralEffect);
+
+    if (config == NULL || !(config->channelsIn == 2 || config->channelsIn == 1)) {
+        return MA_INVALID_ARGS;
+    }
+
+    channelsIn = config->channelsIn;
+    channelsOut = 2; // must be stereo
+
+    baseConfig = ma_node_config_init();
+    baseConfig.vtable = &this->binauralNodeVTable;
+    baseConfig.pInputChannels = &channelsIn;
+    baseConfig.pOutputChannels = &channelsOut;
+
+
+    binauralEffect.audioSettings = config->audioSettings;
+    binauralEffect.context       = config->context;
+    binauralEffect.HRTF          = config->HRTF;
+
+    MA_ZERO_OBJECT(&iplBinauralEffectSettings);
+    iplBinauralEffectSettings.hrtf = binauralEffect.HRTF;
+
+    result = ma_node_init(nodeGraph, &baseConfig, allocationCallbacks, &binauralEffect.baseNode);
+    if (result != MA_SUCCESS) {
+        return result;
     }
 
 
 
-}
-
-void Quack::AudioEngine::destroy() {
-    destroySoundBufferCache(bufferInfo);
-    iplContextRelease(&_context);
-}
-
-void Quack::AudioEngine::processEffect(unsigned int soundId, AudioBuffer& in) {
-
-
-
-    // Should we deinterleave first? would that even work?
-    bufferInfo.inBuffer.numChannels = 0;
-    bufferInfo.inBuffer.numSamples = in.length;
-
-    iplAudioBufferDeinterleave(_context, (float *) in.data,  &bufferInfo.inBuffer);
-
-    IPLBinauralEffectParams  params{};
-
-    //make it come from the right?
-    params.direction = IPLVector3{1.0f, 0.0f, 0.0f};
-    params.hrtf = bufferInfo.hrtf;
-    params.interpolation = IPL_HRTFINTERPOLATION_NEAREST;
-    params.spatialBlend = 1.0f;
-    params.peakDelays = nullptr;
-
-    //iplBinauralEffectApply(bufferInfo.effect, &params, &bufferInfo.inBuffer, &bufferInfo.outBuffer);
-
-
-    in.data = *bufferInfo.outBuffer.data;
-}
-
-unsigned int Quack::AudioEngine::registerSound(Quack::SoundCreateInfo &info) {
-
-
-
-    //iplAudioBufferAllocate(_context, info.channel, info.length, &bufferInfo.inBuffer);
-    iplAudioBufferAllocate(_context, 2, info.length, &bufferInfo.outBuffer);
-
-
-    //iplAudioBufferDeinterleave(_context, in.data, &outBuffer);
-
-    IPLAudioSettings audioSettings{};
-    audioSettings.samplingRate = 44100; // What the hell is this value?
-    audioSettings.frameSize = info.length;
-
-    IPLHRTFSettings hrtfSettings;
-    hrtfSettings.type = IPL_HRTFTYPE_DEFAULT;
-    hrtfSettings.normType = IPL_HRTFNORMTYPE_NONE;
-    hrtfSettings.volume = 1.0f;
-
-    hrtfSettings.sofaData = nullptr;
-    hrtfSettings.sofaFileName = nullptr;
-
-
-    iplHRTFCreate(_context, &audioSettings, &hrtfSettings, &bufferInfo.hrtf);
-
-    if (bufferInfo.hrtf ==  nullptr) {
-        spdlog::error("Could not create HRTF");
-        exit(-104);
+    auto success = iplBinauralEffectCreate(binauralEffect.context, &binauralEffect.audioSettings, &iplBinauralEffectSettings, &binauralEffect.iplEffect);
+    if (success != IPL_STATUS_SUCCESS) {
+        return MA_INVALID_DATA;
     }
 
+    heapSizeInBytes = 0;
 
-    //Convert SDLmixer interleave audio to deinterleave for steamaudio to use.
+    heapSizeInBytes += sizeof(float) * channelsOut * binauralEffect.audioSettings.frameSize; // out buffer
+    heapSizeInBytes += sizeof(float) * channelsIn  * binauralEffect.audioSettings.frameSize; // in buffer
 
+    binauralEffect._heap = ma_malloc(heapSizeInBytes, allocationCallbacks);
+    if (binauralEffect._heap == NULL) {
+        iplBinauralEffectRelease(&binauralEffect.iplEffect);
+        ma_node_uninit(&binauralEffect.baseNode, allocationCallbacks);
+        return MA_OUT_OF_MEMORY;
+    }
 
-    //create a new effect/
-    IPLBinauralEffectSettings  effectSettings{};
-    effectSettings.hrtf = bufferInfo.hrtf;
+    binauralEffect.outBuffer[0] = (float*)binauralEffect._heap;
+    binauralEffect.outBuffer[1] = (float*)ma_offset_ptr(binauralEffect._heap, sizeof(float) * binauralEffect.audioSettings.frameSize);
 
+    ma_uint32 iChannelIn;
+    for (iChannelIn = 0; iChannelIn < channelsIn; iChannelIn += 1) {
+        binauralEffect.inBuffer[iChannelIn] = (float*)ma_offset_ptr(binauralEffect._heap, sizeof(float) * binauralEffect.audioSettings.frameSize * (channelsOut + iChannelIn));
+    }
 
-    iplBinauralEffectCreate(_context, &audioSettings, &effectSettings, &bufferInfo.effect);
-
-
-    spdlog::info("Cooked data for soundeffect");
-    return idCount++;
+    return MA_SUCCESS;
 }
 
-void Quack::AudioEngine::destroySoundBufferCache(Quack::SoundCache &soundBufferInfo) {
-    iplAudioBufferFree(_context, &soundBufferInfo.inBuffer);
-    iplAudioBufferFree(_context, &soundBufferInfo.outBuffer);
-    iplBinauralEffectRelease(&soundBufferInfo.effect);
-    iplHRTFRelease(&soundBufferInfo.hrtf);
+void Quack::AudioEngine::destroyBinauralEffect(Quack::BinauralEffect *binauralEffect,
+                                               const ma_allocation_callbacks *allocationCallbacks) {
+    if (binauralEffect == NULL) {
+        spdlog::error("CPP IS TWEAKING IS SWEAR TO GOD");
+        return;
+    }
+
+    ma_node_uninit(&binauralEffect->baseNode, allocationCallbacks);
+
+    iplBinauralEffectRelease(&binauralEffect->iplEffect);
+
+    ma_free(binauralEffect->_heap, allocationCallbacks);
+
 }
--- Engine/Audio/AudioEngine.h
+++ Engine/Audio/AudioEngine.h
@@ -5,58 +5,76 @@
 #ifndef GAME_AUDIOENGINE_H
 #define GAME_AUDIOENGINE_H
 
-
+#define MINIAUDIO_IMPLEMENTATION
+#include <miniaudio.h>
 #include <phonon.h>
+
+#define FORMAT ma_format_32
+#define CHANNELS 2
+#define SAMPLE_RATE 48000
 
 namespace Quack {
 
+    typedef unsigned int SoundID;
 
-    struct AudioBuffer{
-        int channel;
-        void* data;
-        int length;
+    struct BinauralEffectConfig {
+        ma_node_config nodeConfig;
+        ma_uint32 channelsIn;
+        IPLAudioSettings  audioSettings;
+        IPLContext context;
+        IPLHRTF HRTF;
     };
 
-    struct SoundCreateInfo {
-        int length;
-        int channel = 2;
-        int frameSize;
+    struct BinauralEffect {
+        ma_node_base baseNode;
+        IPLAudioSettings  audioSettings;
+        IPLContext context;
+        IPLHRTF HRTF;
+        IPLBinauralEffect  iplEffect;
+        Quack::Math::Vector3 direction;
+        float* inBuffer[2];
+        float* outBuffer[2];
+        void* _heap;
     };
 
-    struct SoundCache {
-        IPLAudioBuffer inBuffer;
-        IPLAudioBuffer outBuffer;
-        IPLHRTF hrtf;
-        IPLBinauralEffect effect;
-    };
 
     class AudioEngine {
 
 
     public:
 
-        void initialize();
-        void destroy();
+        void Init();
+        void CleanUp();
 
-        unsigned int registerSound(SoundCreateInfo& info);
-
-        void processEffect(unsigned int soundId, AudioBuffer& in);
-
+        SoundID registerSound(const char* filePath);
 
     private:
-        IPLContext _context;
-        SoundCache bufferInfo; // temp
-        unsigned int idCount = 0;
 
-        void destroySoundBufferCache(SoundCache& soundBufferInfo);
+        static void binauralEffectProccessPCMFrames(ma_node* pNode, const float** ppFramesIn, ma_uint32* pFrameCountIn, float** ppFramesOut, ma_uint32* pFrameCountOut);
+
+        BinauralEffectConfig initBinauralEffectConfig(ma_uint32 channelsIn, IPLAudioSettings audioSettings, IPLContext context, IPLHRTF hrtf);
+        ma_result initBinauralEffect(ma_node_graph* nodeGraph, const BinauralEffectConfig* config, const ma_allocation_callbacks* allocationCallbacks, BinauralEffect& binauralEffect);
+
+        void destroyBinauralEffect(BinauralEffect* binauralEffect, const ma_allocation_callbacks * allocationCallbacks);
+
+        ma_node_vtable binauralNodeVTable {
+            AudioEngine::binauralEffectProccessPCMFrames,
+            NULL,
+            1,
+            1,
+            0
+        };
+
+
+
+
+
+        // Dummy
+        ma_sound g_sound;
+
     };
 
 
-
-    struct EffectUserData {
-        AudioEngine* audioEngine;
-        unsigned int soundId;
-    };
 
 }// namespace quack
 
